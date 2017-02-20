5dd200dd1476c567cefa23ed28f2450d34ca348d
Michael Spann <spann@umich.edu>
2017-02-19 21:34:43 -0600

    first commit
    Added movement using vr controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+
+	private Rigidbody rb;
+
+	public float speed;
+
+	private GvrController controller;
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate(){
+
+		if (GvrController.IsTouching) {
+			
+			float moveHorizontal = GvrController.ArmModel.pointerPosition.x;
+			float moveVertical = GvrController.ArmModel.pointerPosition.z;
+			Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+			rb.AddForce (movement * speed);
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("PickUp"))
+		{
+			other.gameObject.SetActive (false);
+		}
+	}
+}
